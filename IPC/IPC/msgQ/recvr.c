1598f5b604bedc91693a4ae87da7a9e1650615c5
Ashiwni Kumar <kittu1796@gmail.com>
2023-04-30 02:38:13 +0200

    Tested message-queue IPC

--- IPC/IPC/msgQ/recvr.c
+++ IPC/IPC/msgQ/recvr.c
@@ -1,58 +1,93 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <mqueue.h>
-#include <errno.h>
+/* 
+* File: sender.c
+* Author: Ashwini Kumar
+* Date: 29.04.23
+*
+* Description: An implementation of Linux MessageQueue receiver. This implementation is 
+*               done reffereing to a course on udemy titled "IPC programming in Linux"
+*/
 
-#define MAX_MESSAGES 10
+//Includes
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<mqueue.h>
+#include<errno.h>
+
+//Macro definitions
 #define MAX_MSG_SIZE 256
-#define MSG_BUFFER_SIZE     (MAX_MSG_SIZE + 10)
+#define MSG_BUFFER_SIZE (MAX_MSG_SIZE+10)
+#define MAX_MESSAGES 10
 #define QUEUE_PERMISSIONS   0660
 
+//main
+int main(int argc, char **argv)
+{
+    //Vairables
+    struct mq_attr attr;
+    mqd_t msgQ_fd;
+    fd_set os_fd_set;
+    char read_buffer[MSG_BUFFER_SIZE];
+    int ret_rx;
 
-int 
-main(int argc, char **argv){
-
-    fd_set readfds;
-    char buffer[MSG_BUFFER_SIZE];
-    int msgq_fd = 0;
-
-    if(argc <= 1){
-        printf("provide a reciepient msgQ name : format </msgq-name>\n");
-        return 0;
+    //Check if the msg queue name was provided as cmdline argument
+    if(argc <= 1)
+    {
+        printf("Provide the receiving msgQ name: format </msgQ-name>\n");
+        exit(EXIT_SUCCESS);
     }
 
-    /*To set msgQ attributes*/
-    struct mq_attr attr;
+    /* Set the attributes */
     attr.mq_flags = 0;
+    attr.mq_curmsgs = 0;
     attr.mq_maxmsg = MAX_MESSAGES;
     attr.mq_msgsize = MAX_MSG_SIZE;
-    attr.mq_curmsgs = 0;
 
-    if ((msgq_fd  = mq_open (argv[1], O_RDONLY | O_CREAT, QUEUE_PERMISSIONS, &attr)) == -1) {
-        printf ("Client: mq_open failed, errno = %d", errno);
-        exit (1);
+    // Open the message queue
+    msgQ_fd = mq_open(argv[1],
+                     O_RDONLY | O_CREAT,
+                     QUEUE_PERMISSIONS,
+                     &attr);
+    if(msgQ_fd == -1)
+    {
+        perror("MQ open failed:");
+        exit(EXIT_FAILURE);
     }
+    printf("Successfully opend the message quesue : %s\n", argv[1]);
 
     while(1){
-        FD_ZERO(&readfds);
-        FD_SET( msgq_fd, &readfds);
-        printf("Reciever blocked on select()....\n");
-        select(msgq_fd + 1, &readfds, NULL, NULL, NULL);
-        if(FD_ISSET(msgq_fd, &readfds)){
-            printf("Msg recvd msgQ %s\n", argv[1]);
-            
-            memset(buffer, 0, MSG_BUFFER_SIZE);
-            if (mq_receive (msgq_fd, buffer, MSG_BUFFER_SIZE, NULL) == -1) {
-                printf ("mq_receive error, errno = %d\n", errno);
-                exit (1);
-            }
-            printf("Msg recieved from Queue = %s\n", buffer);
-        }
-    }
-}
+        //Flush all the elements of the fd_set DS
+        FD_ZERO(&os_fd_set);
 
+        //Add this msgQ fd to the set
+        FD_SET(msgQ_fd, &os_fd_set);
+
+        printf(" Receiver process blocked by selec syscall\n");
+
+        select(msgQ_fd+1, &os_fd_set, NULL, NULL, NULL);
+
+        if(FD_ISSET(msgQ_fd, &os_fd_set))
+        {
+            printf("Message received from sender on queue %s.\n", argv[1]);
+
+            memset(read_buffer, 0, MSG_BUFFER_SIZE);
+
+            ret_rx = mq_receive(msgQ_fd, 
+                                read_buffer,
+                                MSG_BUFFER_SIZE,
+                                NULL);
+            if(ret_rx == -1)
+            {
+                perror("Receiver error: ");
+                exit(EXIT_FAILURE);
+            }
+
+            printf("Msg received from sender = %s\n", read_buffer);
+        }
+    }  
+
+}
