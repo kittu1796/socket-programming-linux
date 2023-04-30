e60915cd5c5b98ab61200182b53502e1daaf9938
Ashiwni Kumar <kittu1796@gmail.com>
2023-04-30 20:15:39 +0200

    mmap for dynamic mem allocation -tested

--- IPC/NewSHM/mmap_as_malloc_Demo1.c
+++ IPC/NewSHM/mmap_as_malloc_Demo1.c
@@ -1,52 +1,78 @@
-#include <stdio.h>
-#include <sys/mman.h>
-#include <errno.h>
-
-/* mmap() has multiple purpose, this is the example where program simply
-do dynamic memory allocation using mmap() instead of malloc() call. There
-is no external data source or memory mapping involved. This example simply
-shows how mmap() can be used as substitution for malloc()/free().
+/* 
+* File: mmap_as_malloc_Demo1.c
+* Author: Ashwini Kumar
+* Date: 30.04.23
+*
+* Description: An implementation of Linux mmap syscall for dynamic memory allocation. This implementation is 
+*               done reffereing to a course on udemy titled "IPC programming in Linux"
 */
 
-#if 0
-void *
-mmap(void *addr, size_t length, int prot, int flags,
-                  int fd, off_t offset);
+//Inlcudes
+#include<stdio.h>
+#include<sys/mman.h> 
+#include<stdlib.h>
 
-int munmap(void *addr, size_t length);
-#endif
+//Maros
+#define NUM_BYTES 5
 
-int 
-main(int argc, char **argv) {
+//function prototype
+static void write_something(int *, int);
+static void read_memory(int *, int);
 
-int N=5;
-int *ptr = mmap ( NULL,     /*   Le the OS choose the starting virtual address of the memory, just
-                                                    like you do not have a control as to what address malloc() will return */
-                            N*sizeof(int),      /* Size of memory in bytes being requested for allocation */ 
-                             PROT_READ |  /* Memory is Readable */
-                             PROT_WRITE, /* Memory is Writable */
-                             MAP_PRIVATE |  /* This memory is not sharable with any other process, use MAP_SHARED instead */
-                             MAP_ANONYMOUS,   /* This memory is not mapped to extern data source, but only RAM by default */
-                             0,     /* FD 0, since no external data source is specified */
-                             0 );  /* offset value as zero, since no extern data source is specified */
+//Global variables
 
-if(ptr == MAP_FAILED){
-    printf("Mapping Failed, errno = %d\n", errno);
-    return 1;
+//Main
+int main(int argc, char **argv)
+{
+    int *mem_ptr = mmap(NULL,
+                        NUM_BYTES*sizeof(int),
+                        PROT_READ | PROT_WRITE,
+                        MAP_PRIVATE | MAP_ANONYMOUS,
+                        0,
+                        0);
+
+    if(mem_ptr == MAP_FAILED)
+    {
+        perror("mmap failed:");
+        exit(EXIT_FAILURE);
+    }
+    printf("mmap successful\n");
+
+
+    //Perform action on memory
+    write_something(mem_ptr, NUM_BYTES);
+    read_memory(mem_ptr, NUM_BYTES);
+
+    //Unmap the memory
+    int err = munmap(mem_ptr, NUM_BYTES*sizeof(int));
+    if(err == -1)
+    {
+        perror("unmap error:");
+        exit(EXIT_FAILURE);
+    }
+    printf("unmap successful\n");
+    return 0;
+
+
 }
 
-for(int i=0; i<N; i++)
-    ptr[i] = i*10;
+static void write_something(int *ptr, int count)
+{
+    printf("Starting to perform action on the memory\n");
 
-for(int i=0; i<N; i++)
-    printf("[%d] ",ptr[i]);
-
-printf("\n");
-int err = munmap(ptr, 10*sizeof(int));
-if(err != 0){
-    printf("UnMapping Failed\n");
-    return 1;
+    for(int i=0; i<count;i++)
+    {
+        ptr[i] = i*10;
+    }
+    printf("Wrtieing Complete\n");
 }
 
-return 0;
+static void read_memory(int *ptr, int count)
+{
+    printf("Reading from memory\n");
+    for(int i=0; i<count; i++)
+    {
+        printf("[%d]", ptr[i]);
+    }
+    printf("\nReading complete\n");
 }
